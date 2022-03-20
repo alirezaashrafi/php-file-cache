be93ec62aa8742a3415ffae9fa41126402ded6bb
alireza <alirezaashrafi.email@gmail.com>
2022-03-21 00:51:39 +0330

    version 2

--- README.md
+++ README.md
@@ -8,6 +8,62 @@ $ composer require alirezaashrafi/php-file-cache
 ```
 
 ## Usage
-```php
+After installation, you can use 'Cache' class in your project
 
+```php
+use AlirezaAshrafi\Cache;
+```
+If class with 'Cache' name already exists you can use AS for rename included class
+```php
+use AlirezaAshrafi\Cache as FileCache;
+```
+
+- Initialization parameters are directory and file name
+- First parameter is absolute path of cache directory
+- Second parameter is the file you want to save results
+
+```php
+$directory = __DIR__ . '/cache-dir';
+$file = 'products';
+
+$cache = new Cache($directory , $file);
+```
+- Directory hierarchy (path/cache-dir/products)
+
+#### Set
+```php
+$cache->set("product-1-name", "Iphone 13");
+$cache->set("product-1-description", "Iphone 13 pro max 256G");
+````
+
+#### Get
+```php
+$product_name = $cache->get("product-1-name", "default value"); // "Iphone 13"
+$product_name = $cache->get("product-2-name", "default value"); // "default value"
+````
+- If cache exists it will return the value ("Iphone 13 pro max")
+- Else if cache not exists default value will return ("default value")
+
+#### Has
+```php
+$cache->has("product-1-name"); // TRUE
+$cache->has("product-2-name"); // FALSE
+````
+- Has function checks the existence of cache in file and memory
+- True or False are return values of has function
+
+#### Delete
+```php
+$cache->delete("product-1-description"); // TRUE
+````
+
+
+#### Get ALL
+```php
+$cache->getAll();
+```
+
+#### Purge (Delete ALL)
+```php
+$cache->purgeAll();
 ```
--- src/Cache.php
+++ src/Cache.php
@@ -1,13 +1,10 @@
 <?php
 
+namespace AlirezaAshrafi;
+
+
 class Cache
 {
-
-    /**
-     * The name of the cache file
-     * @var string
-     */
-    public $cacheDir;
 
     /**
      * The cache data
@@ -16,45 +13,54 @@ class Cache
     protected $data;
 
     /**
-     * Construct or retrieve a cache of data from the given file
+     * @var string
+     */
+    private $directory;
+
+    /**
+     * @var string
+     */
+    private $file;
+
+    /**
+     * Cache constructor.
+     * @param string $directory
      * @param string $file
      */
-    public function __construct($dir = 'key-value-cache2', $name = 'default')
+    public function __construct($directory, $file)
     {
-        $dir = __DIR__ . '/cache/' . $dir;
-
-        if (!file_exists($dir)) {
-            mkdir($dir, 0777, true);
+        if (!file_exists($directory)) {
+            mkdir($directory, 0777, true);
         }
 
-        $this->cacheDir = $dir . '/' . $name;
+        $this->directory = $directory;
+        $this->file = $file;
+
+        if (file_exists($this->getFilePath())) {
+            $this->data = unserialize(file_get_contents($this->getFilePath()));
+        }
     }
 
+
+    function getFilePath()
+    {
+        return $this->directory . '/' . $this->file;
+    }
+
+
+    /**
+     * save changes before close the session
+     */
     function __destruct()
     {
-        file_put_contents($this->getFilePath(), json_encode($this->data));
+        file_put_contents($this->getFilePath(), serialize($this->data));
     }
 
-
-    private function getFilePath()
-    {
-        return $this->cacheDir;
-    }
-
-    public function get($key, $default = '')
-    {
-        if ($this->data == null && file_exists($this->getFilePath())) {
-            $this->data = json_decode(file_get_contents($this->getFilePath()), true);
-        }
-
-        if (isset($this->data[$key])) {
-            return $this->data[$key];
-        }
-
-        return $default;
-    }
-
-
+    /**
+     * @param string $key
+     * @param string $value
+     * @return mixed
+     */
     public function set($key, $value)
     {
         $this->data[$key] = $value;
@@ -62,4 +68,36 @@ class Cache
         return $value;
     }
 
+    public function get($key, $default = null)
+    {
+        if (isset($this->data[$key])) {
+            return $this->data[$key];
+        }
+
+        return $default;
+    }
+
+    public function has($key)
+    {
+        return isset($this->data[$key]);
+    }
+
+    public function delete($key)
+    {
+        unset($this->data[$key]);
+
+        return true;
+    }
+
+    public function getAll()
+    {
+        return $this->data;
+    }
+
+    public function purgeAll()
+    {
+        $this->data = array();
+
+        return true;
+    }
 }
